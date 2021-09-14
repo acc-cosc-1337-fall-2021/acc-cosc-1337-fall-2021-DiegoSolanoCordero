9bd1929452be74d1cd963a08b968f399c52030dd
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-09-14 17:38:52 -0500

    commit

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,15 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
-
+double get_sales_tax(double meal_ammount)
+{
+	double tax_rate=0.0675;
+	return meal_ammount*tax_rate;
+}
+double get_tip_ammount(double meal_ammount, double tip_rate)
+{
+	return meal_ammount*tip_rate;
+}
 
 
 
