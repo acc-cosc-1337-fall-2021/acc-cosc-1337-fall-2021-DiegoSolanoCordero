e27e44a44bbdc563534890ec9da6656282307c2d
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-09-21 16:52:59 -0500

    comit

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,11 +1,9 @@
 //write include required statements
 #include <string>
 
-
 //Write a function(s) prototype  here
-using std::string
-string get_letter_grade_using_if(int grade);
-string get_letter_grade_using_switch(int grade)
+std::string get_letter_grade_using_if(int grade);
+std::string get_letter_grade_using_switch(int grade);
 
 
 
