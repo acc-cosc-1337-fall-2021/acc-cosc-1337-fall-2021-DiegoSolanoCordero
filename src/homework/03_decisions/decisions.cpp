e1743880a4c47bbd7f13a7c1cdcb391789fac1d4
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-09-21 15:19:16 -0500

    commit

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,57 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade);
+{
+    if(grade >= 90 && grade <=100 )
+    {
+        return "A";
+    }
+    else if (grade >= 80 && grade <=89 )
+    {
+        return "B";
+    }
+    else if (grade >= 70 && grade <=79 )
+    {
+        return "C";
+    }
+    else if (grade >= 60 && grade <=69 )
+    {
+        return "D";
+    }
+    else if (grade >= 00 && grade <=59 )
+    {
+        return "F";
+    }
+    else
+    {
+        return "Invalid grade";
+    }
+}
+string get_letter_grade_using_switch(int grade);
+{
+    switch(int grade)
+    {
+        case grade >= 90 && grade <=100 : 
+        return "A";
+        break;
+        case grade >= 80 && grade <=89 :
+        return "B";
+        break;
+        case grade >= 70 && grade <=79 :
+        return "C";
+        break;
+        case grade >= 60 && grade <=69:
+        return "D";
+        break;
+        case grade >= 00 && grade <=59:
+        return "F";
+        break;
+        default:
+        return "Invalid grade";
+        break;
+
+    }
+    return result
+}
