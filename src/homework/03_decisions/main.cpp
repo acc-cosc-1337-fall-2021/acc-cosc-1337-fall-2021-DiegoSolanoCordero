6849a598d0872f15ced8ceef6efa1227e1607ebe
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-09-22 09:10:01 -0500

    commit

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,36 @@
 //write include statements
+#include "decisions.h"
+#include "iostream"
+#include <string>
+using std::cout;
+using std::cin; 
+using std::string;
 
 int main() 
 {
+	int grade;
+	int Menu;
+	std::string result;
+	cout<<"Enter your grade: ";//Ask for grade
+	cin>>grade;
+	cout<<"Press 1 to find grade with If. Press 2 to find grade with switch: ";//Ask for calculating method
+	cin>>Menu;
+	switch(Menu)
+    {
+        case 1: 
+        cout<<"Option 1 selected";
+		result= get_letter_grade_using_if(grade);
+		cout<<"Your grade would be:"<<result<<"\n";
+        break;
+        case 2:
+        cout<< "Option 2 selected";
+		result= get_letter_grade_using_switch(grade);
+		cout<<"Your grade would be:"<<result<<"\n";
+        break;
+		default:
+        cout<< "Invalid grade";
+        break;
+
+	
 	return 0;
 }
