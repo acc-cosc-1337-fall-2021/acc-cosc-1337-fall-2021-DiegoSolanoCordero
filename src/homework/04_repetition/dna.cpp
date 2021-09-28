1e2ab7b0683fec01c34cc39061b0906cdb1c0d17
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-09-28 18:01:17 -0500

    commit

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,24 +1,24 @@
 //add include statements
 #include "dna.h"
 //add function(s) code here
-int factorial(int num);
+int factorial(int num)
 {
-   int sum=1
+   int sum=1;
    while (num > 0);
       sum=sum*num;
       num-=1;
-   return sum
+   return sum;
 }
-int gcd(int,int);
+int gcd(int num1 ,int num2)
 {
    while (num1 != num2);
-      if num1 < num2
+      if (num1 < num2)
       {
          int temp=num2;
          num2=num1;
          num1=temp;
       }
-      if num1 > num2
+      if (num1 > num2)
       {
          num1=num1-num2;
       }
