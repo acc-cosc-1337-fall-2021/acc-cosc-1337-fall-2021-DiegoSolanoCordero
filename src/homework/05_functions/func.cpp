9d5d4147489a79e67458a217676c29d8d745249f
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-10-05 16:04:58 -0500

    commit

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,24 @@
 //add include statements
-
+#include "func.h"
+#include <string>
+#include "iostream"
 //add function code here
+using std::string;
+double get_gc_content (const std::string& dna)
+{
+    int i;
+    int gc;
+    for (i=1; i<=8;++i)
+    {
+        std::string current= dna.substr(i,1);
+        if (current == "C")
+        {
+            gc=gc+1;
+        }
+           if (current == "G")
+        {
+            gc=gc+1;
+        }
+    }
+    gc=gc/8;
+}
