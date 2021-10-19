94d67fe54f9848f1c0dcc361fcb6eb35dcbd5a44
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-10-19 17:56:11 -0500

    commit

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,22 @@
-
+#include <string>
+#include <vector>
+#include "iostream"
+#include "tic_tac_toe.h"
+using std::string;
+using std::cout;
+using std::cin;
 int main() 
 {
-
+	std::string first_player;
+	int position;
+	int loop;
+	TicTacToe instance;
+	cout<<"Press X or Y to"<<"\n";
+	cin>>first_player;
+	instance.start_game(first_player);
+	while (loop=0)
+	instance.display_board();
+	instance.mark_board(position);
+	
 	return 0;
 }
