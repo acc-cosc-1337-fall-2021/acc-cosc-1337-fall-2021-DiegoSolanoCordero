e71d91447279b47d3a3a615741fb0a634fea9ca0
diegosolanoc <diego.solanocordero@g.austincc.edu>
2021-10-26 14:27:57 -0500

    commit

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -8,12 +8,28 @@ using std::cout;
 //return check_board_full function return value
 bool TicTacToe::game_over()
 {
+    /*bool check;
+    check=check_column_win();
+    if (check=true)
+    return check;
+    else
+    check=check_row_win();
+    if (check=true)
+    return check;
+    else
+    check=check_diagonal_win();
+    if (check=true)
+    return check;
+    else*/
     return check_board_full();
 }
 //set first player value to player
 void TicTacToe::start_game(std::string first_player)
 {
-    player=first_player;
+    if (first_player=="X"||first_player=="x")
+    player="X";
+    else if (first_player=="O"||first_player=="o")
+    player="O";
     TicTacToe::clear_board();
 }
 //Mark position -1	
@@ -35,7 +51,11 @@ void TicTacToe::display_board() const
     cout<<"["<<pegs[3]<<"]"<<"["<<pegs[4]<<"]"<<"["<<pegs[5]<<"]"<<"\n";
     cout<<"["<<pegs[6]<<"]"<<"["<<pegs[7]<<"]"<<"["<<pegs[8]<<"]"<<"\n";
 }
-
+// Find winner
+string TicTacToe::get_winner()
+{
+    return winner;
+}
 //If Player X or O then O or X
 void TicTacToe::set_next_player()
 {
@@ -55,7 +75,6 @@ bool TicTacToe::check_board_full()
         if (itr!="X" & itr!="O" & itr!="x" & itr!="o")
         empty=empty+1;
     }
-
     if (empty>0) 
     over=false;   
     return over;
@@ -68,4 +87,44 @@ void TicTacToe::clear_board()
     //{
       //  itr=" ";
     //}
+}
+//Check for column winners
+bool TicTacToe:: check_column_win()
+{
+    if (pegs[0]=="X" && pegs[3]=="X" && pegs[6]=="X" ||pegs[1]=="X" && pegs[4]=="X" && pegs[7]=="X"||pegs[2]=="X" && pegs[5]=="X" && pegs[8]=="X")
+    return true;
+    else if (pegs[0]=="O" && pegs[3]=="O" && pegs[6]=="O" ||pegs[1]=="O" && pegs[4]=="O" && pegs[7]=="O"||pegs[2]=="O" && pegs[5]=="O" && pegs[8]=="O")
+    return true;
+    else
+    return false;
+    
+}
+//Check for row winners
+bool TicTacToe:: check_row_win()
+{
+    if (pegs[0]=="X" && pegs[1]=="X" && pegs[2]=="X" ||pegs[3]=="X" && pegs[4]=="X" && pegs[5]=="X"||pegs[6]=="X" && pegs[7]=="X" && pegs[8]=="X")
+    return true;
+    else if (pegs[0]=="O" && pegs[1]=="O" && pegs[2]=="O" ||pegs[3]=="O" && pegs[4]=="O" && pegs[5]=="O"||pegs[6]=="O" && pegs[7]=="O" && pegs[8]=="O")
+    return true;
+    else
+    return false;
+    
+}
+//Check for diagonal winners
+bool TicTacToe:: check_diagonal_win()
+{
+    if (pegs[0]=="X" && pegs[4]=="X" && pegs[8]=="X" ||pegs[6]=="X" && pegs[4]=="X" && pegs[2]=="X")
+    return true;
+    else if (pegs[0]=="O" && pegs[4]=="O" && pegs[8]=="O" ||pegs[6]=="O" && pegs[4]=="O" && pegs[2]=="O")
+    return true;
+    else
+    return false;
+}
+//Set winner
+void TicTacToe:: set_winner()
+{
+    if (player=="X")
+    winner="O";
+    else if (player=="O")
+    winner="X";
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -11,10 +11,16 @@ class TicTacToe
         void mark_board(int position);
         void display_board()const;
         std::string get_player()const;
+        std::string get_winner();
     private:
         std::string player;
         std::vector<std::string> pegs= std::vector<std::string>(9," ");
+        std::string winner;
         void set_next_player();
         bool check_board_full();
         void clear_board();
+        bool check_column_win();
+        bool check_row_win();
+        bool check_diagonal_win();
+        void set_winner();
 };
